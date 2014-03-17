cdaa7479078b964f810ce2c19853d44f29df1d7b
maartn <maarten.christiaen@gmail.com>
2014-03-17 09:15:43 +0100

    adapted r script
    0 indexing

--- script.r
+++ script.r
@@ -1,15 +1,14 @@
 baseline<-read.table("baseline.txt",header=TRUE)
 freetime<-read.table("freetime.txt",header=TRUE)
 workload<-read.table("workload.txt",header=TRUE)
-f<-function(x) x/baseline$sum[1]
 rel_freetime<-freetime$sum/baseline$sum[1]
 rel_workload<-workload$sum/baseline$sum[1]
 rel_baseline<-baseline$sum/baseline$sum[1]
 g_range <- range(0, rel_freetime,rel_baseline, rel_workload)
 pdf("relative_comparisson.pdf")
-plot(rel_baseline, type="o",col="black", ylim=g_range,ann=FALSE)
-lines(rel_workload, type="o",col="red")
-lines(rel_freetime,type="o",col="blue")
+plot(x=c(0:(length(rel_baseline)-1)),y=rel_baseline, type="o",col="black", ylim=g_range,ann=FALSE)
+lines(x=c(0:(length(rel_baseline)-1)),y=rel_workload, type="o",col="red")
+lines(x=c(0:(length(rel_baseline)-1)),y=rel_freetime,type="o",col="blue")
 title(main="Relative comparisson")
 title(xlab="Number of failures")
 title(ylab="Relative cost")
